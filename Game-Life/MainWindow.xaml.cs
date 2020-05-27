fd63b18fb8d1be3f715fc04bb3bbf04cf8bffd84
Sergey Vitovskiy <SergeyVitovskiy@yandex.ru>
2020-05-27 23:30:00 +0500

    Таймер

--- Game-Life/MainWindow.xaml.cs
+++ Game-Life/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,7 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Windows.Threading;
 namespace Game_Life
 {
     /// <summary>
@@ -24,5 +25,41 @@ namespace Game_Life
         {
             InitializeComponent();
         }
+        DispatcherTimer timer = new DispatcherTimer();//Задаем таймер
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer.Interval = TimeSpan.FromSeconds(0.1);//Интервал таймера
+            timer.Tick += timerFunc;//Присваиваем функцию          
+        }
+
+        private int increment = 0;
+        private void timerFunc(object sender, EventArgs e)
+        {
+            increment++;
+
+            TimerLabel.Content = increment.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();//Запускаем таймер
+        }      
     }
 }
+//public MainWindow()
+//{
+//    InitializeComponent();
+//    ellipse.Width = 20;
+//    ellipse.Height = 20;
+//    ellipse.Fill = Brushes.Black;
+
+//    myCanvas.Children.Add(ellipse);
+//}
+
+//Ellipse ellipse = new Ellipse();
+//private void Button_Click(object sender, RoutedEventArgs e)
+//{
+//    Canvas.SetLeft(ellipse, 50);
+//    Canvas.SetTop(ellipse, 70);
+
+//}
